4982f8b12af0679f4ad8557977f52420efcc0db0
Nicholas Halka <88tontos@gmail.com>
2026-09-13 10:24:29 -0400

    feat(web): offer mark private on every owned-asset select bar
    favorites, archive, recently added, tags, person, map, folders and the
    memory viewer get the same mark/unmark entries as the photos page

--- web/src/routes/(user)/folders/[[photos=photos]]/[[assetId=id]]/+page.svelte
+++ web/src/routes/(user)/folders/[[photos=photos]]/[[assetId=id]]/+page.svelte
@@ -17,6 +17,7 @@
   import DeleteAssets from '$lib/components/timeline/actions/DeleteAssetsAction.svelte';
   import DownloadAction from '$lib/components/timeline/actions/DownloadAction.svelte';
   import FavoriteAction from '$lib/components/timeline/actions/FavoriteAction.svelte';
+  import SetPrivateAction from '$lib/components/timeline/actions/SetPrivateAction.svelte';
   import SetVisibilityAction from '$lib/components/timeline/actions/SetVisibilityAction.svelte';
   import TagAction from '$lib/components/timeline/actions/TagAction.svelte';
   import AssetSelectControlBar from '$lib/components/timeline/AssetSelectControlBar.svelte';
@@ -151,6 +152,7 @@
         <ChangeDescription menuItem />
         <ChangeLocation menuItem />
         <ArchiveAction menuItem unarchive={assetMultiSelectManager.isAllArchived} onArchive={triggerAssetUpdate} />
+        <SetPrivateAction menuItem onSetPrivate={triggerAssetUpdate} onRemove={triggerAssetUpdate} />
         <SetVisibilityAction menuItem onVisibilitySet={handleSetVisibility} />
         {#if authManager.preferences.tags.enabled && assetMultiSelectManager.isAllUserOwned}
           <TagAction menuItem />
