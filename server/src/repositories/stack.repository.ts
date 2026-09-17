6519b08a7a47be3283be082731bba59fc49b6fd4
Nicholas Halka <88tontos@gmail.com>
2026-09-16 22:51:44 -0400

    fix(server): hide children of hidden tags in duplicate and stack asset responses

--- server/src/repositories/stack.repository.ts
+++ server/src/repositories/stack.repository.ts
@@ -6,7 +6,13 @@ import { columns } from 'src/database';
 import { DummyValue, GenerateSql } from 'src/decorators';
 import { DB } from 'src/schema';
 import { StackTable } from 'src/schema/tables/stack.table';
-import { asUuid, isTagHidden, PrivateScope, withDefaultVisibility, withPrivateScope } from 'src/utils/database';
+import {
+  asUuid,
+  PrivateScope,
+  withDefaultVisibility,
+  withEffectiveTagColumns,
+  withPrivateScope,
+} from 'src/utils/database';
 
 export interface StackSearch {
   ownerId: string;
@@ -32,8 +38,7 @@ const withAssets = (eb: ExpressionBuilder<DB, 'stack'>, scope: PrivateScope, wit
           jsonArrayFrom(
             eb
               .selectFrom('tag')
-              .select(['tag.id', 'tag.value', 'tag.createdAt', 'tag.updatedAt', 'tag.color', 'tag.parentId'])
-              .select((eb) => isTagHidden(eb, 'tag.id').$castTo<boolean>().as('isHidden'))
+              .select(withEffectiveTagColumns)
               .innerJoin('tag_asset', 'tag.id', 'tag_asset.tagId')
               .whereRef('tag_asset.assetId', '=', 'asset.id'),
           ).as('tags'),
